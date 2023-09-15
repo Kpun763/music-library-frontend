eaf139be410d382e3481836a093d8d50d0be219c
Kyle Washington <kmw1993@msn.com>
2023-09-15 11:10:28 -0400

    Added async functioin. Also created new components and ran import axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import axios from "axios";
+
 
 function App() {
+
+  const fetchMusic = async () => {
+    const response = await axios.get("https://localhost:7073/api/MusicLibrary")
+    console.log(response)
+  }
+
   return (
     <div className="App">
       <Header />
+      <button onClick={fetchMusic}>Add Music</button>
     </div>
   );
 }
